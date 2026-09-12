7cf6b779491de6ea9946bf93ba5545dae2450c76
Khushi <khushijangra566@gmail.com>
2026-09-12 23:14:41 +0530

    pomodaro css update

--- pomodaroClock/script.js
+++ pomodaroClock/script.js
@@ -1,22 +1,21 @@
 const container = document.getElementById("container");
 const upper = document.getElementById("upper");
-const lower = document.getElementById("lower");
-const session = document.getElementById("session");
-const times = document.getElementById("times");
-const sessionTime = document.getElementById("session-time");
-const breakTime = document.getElementById("break-time");
+
 const sessionText = document.getElementById("session-time1");
 const breakText = document.getElementById("break-time1");
 const sessionSpan = document.getElementById("session-span");
+
 const start = document.getElementById("start");
+
 const plusSessionBtn = document.getElementById("plus-session");
 const minusSessionBtn = document.getElementById("minus-session");
 const plusBreakBtn = document.getElementById("plus-break");
 const minusBreakBtn = document.getElementById("minus-break");
+
 const modeText = document.getElementById("mode-text");
 
 
-// variable
+// timer state
 
 let min = 25;
 let sec = 0;
@@ -24,55 +23,11 @@ let timer = null;
 let mode = "session";
 let hour = 0;
 
-// function
-
-function settimer() {    if (timer !== null) {
-        return;
-    }
-    timer = setInterval(function () {
-        if (sec > 0) { sec--; }
-        else if (min > 0) {
-            min--;
-            sec = 59;
-        }
-        else if (hour > 0) {
-            hour--;
-            min = 59;
-            sec = 59;
-        }
-        else {
-            clearInterval(timer);
-            timer = null;
-            if (mode === "session") {
-
-                let breakMinutes = Number(breakText.innerText);
-                min = breakMinutes;
-                sec = 0;
-                hour = 0;
-                mode = "break";
-                modeText.innerText = "Break";
-
-                settimer();
-
-            }
-            else {
-
-                let sessionMinutes = Number(sessionText.innerText);
-                mode = "session";
-                min = sessionMinutes;
-                sec = 0;
-                hour = 0;
-                modeText.innerText = "Session"
-                let sessionNumber = Number(sessionSpan.innerText);
-                sessionNumber++;
-                sessionSpan.innerText = sessionNumber;
-                settimer();
-
-            }
-        } showFunction();
-    }, 1000
-    )
-}
+// display functions
+function showFunction() {
+    upper.innerText =
+        `${String(hour).padStart(2, "0")}:${String(min).padStart(2, "0")}:${String(sec).padStart(2, "0")}`;
+};
 
 function updateTimer(value){
      if (value >= 60) {
@@ -87,87 +42,175 @@ function updateTimer(value){
            showFunction();
 }
 
-function showFunction() {
-    upper.innerText =
-        `${String(hour).padStart(2, "0")}:${String(min).padStart(2, "0")}:${String(sec).padStart(2, "0")}`;
-};
+// timer functions
+
+function stopTimer() {
+clearInterval(timer);
+timer = null;
+}
+function startTimer() {
+if (timer !== null) {
+return;
+}
+timer = setInterval(function () {
+    if (sec > 0) {
+        sec--;
+    }
+    else if (min > 0) {
+        min--;
+        sec = 59;
+    }
+    else if (hour > 0) {
+        hour--;
+        min = 59;
+        sec = 59;
+    }
+    else {
+        stopTimer();
+        switchMode();
+        startTimer();
+        return;
+    }
+
+    showFunction();
+
+}, 1000);
+
+}
+function switchMode() {
+    start.innerText = "Pause";
+if (mode === "session") {
+mode = "break";
+modeText.innerText = "Break";
+
+    updateTimer(Number(breakText.innerText));
+}
+else {
+    mode = "session";
+    modeText.innerText = "Session";
+
+    updateTimer(Number(sessionText.innerText));
+
+    let sessionNumber = Number(sessionSpan.innerText);
+    sessionNumber++;
+
+    sessionSpan.innerText = sessionNumber;
+}
+
+}
+// Button state
+
+function disableTimeButtons() {
+plusBreakBtn.disabled = true;
+minusBreakBtn.disabled = true;
+plusSessionBtn.disabled = true;
+minusSessionBtn.disabled = true;
+}
+
+function enableTimeButtons() {
+plusBreakBtn.disabled = false;
+minusBreakBtn.disabled = false;
+plusSessionBtn.disabled = false;
+minusSessionBtn.disabled = false;
+}
+// Reset
+
+function resetTimer() {
+stopTimer();
+
+mode = "session";
+min = 25;
+sec = 0;
+hour = 0;
+
+sessionSpan.innerText = 0;
+modeText.innerText = "Session";
+
+sessionText.innerText = 25;
+breakText.innerText = 5;
+
+start.innerText = "Start";
+
+enableTimeButtons();
 showFunction();
-//event listener
+
+}
+// event listner
 container.addEventListener("click", function (event) {
-    if (event.target.matches("#plus-session")) {
-        let value = Number(sessionText.innerText);
-        value++;
+
+if (event.target.matches("#plus-session")) {
+    let value = Number(sessionText.innerText);
+
+    value++;
+    sessionText.innerText = value;
+
+    if (mode === "session") {
+        updateTimer(value);
+    }
+}
+
+else if (event.target.matches("#minus-session")) {
+    let value = Number(sessionText.innerText);
+
+    if (value > 0) {
+        value--;
         sessionText.innerText = value;
+
         if (mode === "session") {
             updateTimer(value);
         }
     }
-    if (event.target.matches("#minus-session")) {
-        let value = Number(sessionText.innerText);
-        if (value > 0) {
-            value--;
-            sessionText.innerText = value;
-            if (mode === "session") {
-            updateTimer(value);}
-        }
+}
+
+else if (event.target.matches("#plus-break")) {
+    let value = Number(breakText.innerText);
+
+    value++;
+    breakText.innerText = value;
+
+    if (mode === "break") {
+        updateTimer(value);
     }
-    if (event.target.matches("#plus-break")) {
-        let value2 = Number(breakText.innerText);
-        value2++;
-        breakText.innerText = value2;
+}
+
+else if (event.target.matches("#minus-break")) {
+    let value = Number(breakText.innerText);
+
+    if (value > 0) {
+        value--;
+        breakText.innerText = value;
+
         if (mode === "break") {
-            updateTimer(value2);
+            updateTimer(value);
         }
     }
-    if (event.target.matches("#minus-break")) {
-        let value2 = Number(breakText.innerText);
-        if (value2 > 0) {
-            value2--;
-            breakText.innerText = value2;
-            if (mode === "break") {
-               updateTimer(value2);
-            }
-        }
+}
 
-    }
-    if (event.target.matches("#reset")) {
-        clearInterval(timer);
-        timer = null;
-        mode = "session";
-        sessionSpan.innerText = 0;
-        modeText.innerText = "Session";
+else if (event.target.matches("#start")) {
 
+    if (timer !== null) {
+        stopTimer();
 
-        breakText.innerText = 5;
-        sessionText.innerText = 25;
         start.innerText = "Start";
-        min = 25;
-        sec = 0;
-        hour = 0;
-        plusBreakBtn.disabled = false;
-        minusBreakBtn.disabled = false;
-        plusSessionBtn.disabled = false;
-        minusSessionBtn.disabled = false;
-        showFunction();
+        enableTimeButtons();
     }
-    if (event.target.matches("#start")) {
-        if (start.innerText === "Pause") {
-            // plusBreakBtn.disabled=true;
-            start.innerText = "Start";
-            plusBreakBtn.disabled = false;
-            minusBreakBtn.disabled = false;
-            plusSessionBtn.disabled = false;
-            minusSessionBtn.disabled = false;
-            clearInterval(timer);
-            timer = null;
-        }
-        else {
-            start.innerText = "Pause";
-            plusBreakBtn.disabled = true;
-            minusBreakBtn.disabled = true;
-            plusSessionBtn.disabled = true;
-            minusSessionBtn.disabled = true;
-            settimer();
-        }
+    else {
+        startTimer();
+
+        start.innerText = "Pause";
+        disableTimeButtons();
     }
-})
+}
+
+else if (event.target.matches("#reset")) {
+    resetTimer();
+}
+
+});
+
+// Initial display
+
+showFunction();
+
+
+
